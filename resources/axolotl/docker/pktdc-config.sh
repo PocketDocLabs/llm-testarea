4b8490c457a1b9297ad5213a636ae2d40f923d2d
pyropanda5 <pyropanda5@gmail.com>
2023-06-09 22:20:22 +0000

    added teminal config options

--- resources/axolotl/docker/pktdc-config.sh
+++ resources/axolotl/docker/pktdc-config.sh
@@ -52,7 +52,7 @@ JUPYTER_PASSWORD=${JUPYTER_PASSWORD:-"axolotl"}
 
 echo "Launching Jupyter Lab with nohup..."
 cd /
-nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=$WORKSPACE &
+nohup jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.allow_origin=* --ServerApp.preferred_dir=$WORKSPACE &
 
 
 
